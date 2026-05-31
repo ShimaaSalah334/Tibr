fc0d57b863f2dcc416b36c212313b9babb11b80a
Shimaa Salah <shimaaasalah2002@gmail.com>
2026-05-31 17:16:06 +0300

    Implement product details component logic

--- src/app/pages/guest/product-details/product-details.ts
+++ src/app/pages/guest/product-details/product-details.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ProductService } from '../../../core/services/product-service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProductDetails } from '../../../core/interfaces/iproduct';
 
 @Component({
   selector: 'app-product-details',
@@ -7,5 +10,30 @@ import { Component } from '@angular/core';
   styleUrl: './product-details.css',
 })
 export class ProductDetails {
+private productService = inject(ProductService);
+private route          = inject(ActivatedRoute);
+private router         = inject(Router);
+
+  // Product state 
+  product      = signal<IProductDetails | null>(null);
+  isLoading    = signal<boolean>(false);
+  error        = signal<string | null>(null);
+
+  //  Data loaders 
+  private loadProduct(id: number): void {
+    this.isLoading.set(true);
+    this.error.set(null);
+
+    this.productService.getProductById(id).subscribe({
+      next: (data) => {
+        this.product.set(data);
+        this.isLoading.set(false);
+      },
+      error: (err) => {
+        this.error.set('Product not found');
+        this.isLoading.set(false);
+      }
+    });
+  }
 
 }
